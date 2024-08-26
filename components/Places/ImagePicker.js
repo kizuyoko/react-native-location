39cbdcfe9fd2c3818127eafa2b3b6f5b3b90c54c
kizuyoko <kizuyoko@gmail.com>
2024-08-26 16:04:05 +0200

    Image Preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,7 +1,11 @@
-import { View, Button, Alert } from "react-native";
+import { View, Button, Alert, Image, Text, StyleSheet } from "react-native";
 import { launchCameraAsync, useCameraPermissions, PermissionStatus } from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constans/colors";
 
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
 
   async function varifyPermissions() {
@@ -34,16 +38,20 @@ function ImagePicker() {
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
-
     });
-    console.log(image)
+    
+    setPickedImage(image.assets[0].uri);
+  }
+
+  let imagePreview = <Text>No image</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />;
   }
 
   return (
     <View>
-      <View>
-
-      </View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
       <Button 
         title='Take Image'
         onPress={takeImageHandler}
@@ -53,3 +61,20 @@ function ImagePicker() {
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover', 
+  }
+});
