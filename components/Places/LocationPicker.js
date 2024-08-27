0834346c54900d243fb4f5662f0fb880e8a328e3
kizuyoko <kizuyoko@gmail.com>
2024-08-27 18:30:57 +0200

    from lat and lng to address

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -3,7 +3,7 @@ import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } f
 import OutlinedButton from "../../UI/OutlinedButton";
 import { Colors } from "../../constans/colors";
 import { useEffect, useState } from "react";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 import { useNavigation, useRoute, useIsFocused } from "@react-navigation/native";
 
 function LocationPicker({ onPickLocation }) {
@@ -26,7 +26,14 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(pickedLocation.lat, pickedLocation.lng);
+        onPickLocation({ ...pickedLocation, address: address });
+      } 
+    }
+    
+    handleLocation();
   }, [pickedLocation, onPickLocation]);
 
   async function varifyPermissions() {
