f95bf9a3b5f875974a6eaaf622a1e34fd8e79fd9
kizuyoko <kizuyoko@gmail.com>
2024-08-27 15:16:13 +0200

    Start location picker - install expo location, permission, google map api, Location Preview

---
+++ components/Places/LocationPicker.js
@@ -0,0 +1,103 @@
+import { StyleSheet, View, Alert, Image, Text } from "react-native";
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from "expo-location";
+import OutlinedButton from "../../UI/OutlinedButton";
+import { Colors } from "../../constans/colors";
+import { useState } from "react";
+import { getMapPreview } from "../../util/location";
+
+function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState();
+
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+
+  async function varifyPermissions() {
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions!', 
+        'You need to grant camera permissions to use this app.'
+      );
+
+      return false;
+    }
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await varifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    })
+  }
+
+  function pickOnMapHandler() {
+
+  }
+
+  let locationPreview = <Text>No location</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image 
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }} 
+      />
+    );
+  }
+
+  return (
+    <View>
+      <View style={styles.mapPreview}>
+        {locationPreview}
+      </View>
+      <View style={styles.actions}>
+        <OutlinedButton 
+          icon='location'
+          onPress={getLocationHandler}
+        >Locate User</OutlinedButton>
+        <OutlinedButton 
+          icon='map'
+          onPress={pickOnMapHandler}
+        >Pick on Map</OutlinedButton>
+      </View>
+    </View>
+  );
+}
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
+});
--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
 import { useState } from "react";
 import { Colors } from "../../constans/colors";
 import ImagePicker from "./ImagePicker";
+import LocationPicker from "./LocationPicker";
 
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState();
@@ -22,6 +23,7 @@ function PlaceForm() {
         />
       </View>
       <ImagePicker />
+      <LocationPicker />
     </ScrollView>
   ); 
  }
