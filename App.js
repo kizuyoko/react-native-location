bf7b293743e2f74ef0a0070ff0218d9a7c40e162
kizuyoko <kizuyoko@gmail.com>
2024-08-28 14:53:24 +0200

    SQLite install, got error with db config

--- App.js
+++ App.js
@@ -6,10 +6,27 @@ import AddPlace from './screens/AddPlace';
 import IconButton from './UI/IconButton';
 import { Colors } from './constans/colors';
 import Map from './screens/Map';
+//import { useEffect, useState } from 'react';
+//import { init } from './util/database';
+//import AppLoading from 'expo-app-loading';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+/*  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init().then(() => {
+      setDbInitialized(true)
+    }).catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  if (!dbInitialized) {
+    return <AppLoading />;
+  }
+*/
   return (
     <>
       <StatusBar style='dark' />
