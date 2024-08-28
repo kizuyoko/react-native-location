e2d400b18d24948b30dc35c19d23ff37db874a26
kizuyoko <kizuyoko@gmail.com>
2024-08-28 10:49:12 +0200

    Remove dummy code

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyD1359pc6D1q0kpmYBTi_JLaMPb1SGMcHw';
+import { GOOGLE_API_KEY } from '@env';
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng},
